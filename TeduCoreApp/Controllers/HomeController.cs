0c12e66ac8534661ff99e20986ab8f0b6c91b9a5
Administrator <Administrator@WIN-A8E5KERI8HT>
2019-07-26 13:15:14 +0700

    //[Authorize]

--- TeduCoreApp/Controllers/HomeController.cs
+++ TeduCoreApp/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace TeduCoreApp.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
