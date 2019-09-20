00c5b7b917ba03d5baac44905a52d818ff6e7aa8
Administrator <Administrator@WIN-A8E5KERI8HT>
2019-09-20 17:16:48 +0700

    lesson & 29,30

--- TeduCoreApp/wwwroot/app/controllers/product/index.js
+++ TeduCoreApp/wwwroot/app/controllers/product/index.js
@@ -3,9 +3,24 @@
         loadCategories();
         loadData();
         registerEvents();
+        registerControls();
     }
 
     function registerEvents() {
+        //Init validation
+        $('#frmMaintainance').validate({
+            errorClass: 'red',
+            ignore: [],
+            lang: 'vi',
+            rules: {
+                txtNameM: { required: true },
+                ddlCategoryIdM: { required: true },
+                txtPriceM: {
+                    required: true,
+                    number: true
+                }
+            }
+        });
         //todo: binding events to controls
         $('#ddlShowPage').on('change', function () {
             tedu.configs.pageSize = $(this).val();
@@ -26,24 +41,171 @@
             $('#modal-add-edit').modal('show');
 
         });
-    }
-    function loadCategories() {
-        $.ajax({
-            type: 'GET',
-            url: '/admin/product/GetAllCategories',
-            dataType: 'json',
-            success: function (response) {
-                var render = "<option value=''>--Select category--</option>";
-                $.each(response, function (i, item) {
-                    render += "<option value='" + item.Id + "'>" + item.Name + "</option>"
-                });
-                $('#ddlCategorySearch').html(render);
-            },
-            error: function (status) {
-                console.log(status);
-                tedu.notify('Cannot loading product category data', 'error');
-            }
+        $('body').on('click', '.btn-edit', function (e) {
+            e.preventDefault();
+            var that = $(this).data('id');
+            $.ajax({
+                type: "GET",
+                url: "/Admin/Product/GetById",
+                data: { id: that },
+                dataType: "json",
+                beforeSend: function () {
+                    tedu.startLoading();
+                },
+                success: function (response) {
+                    var data = response;
+                    $('#hidIdM').val(data.Id);
+                    $('#txtNameM').val(data.Name);
+                    initTreeDropDownCategory(data.CategoryId);
+
+                    $('#txtDescM').val(data.Description);
+                    $('#txtUnitM').val(data.Unit);
+
+                    $('#txtPriceM').val(data.Price);
+                    $('#txtOriginalPriceM').val(data.OriginalPrice);
+                    $('#txtPromotionPriceM').val(data.PromotionPrice);
+
+                    // $('#txtImageM').val(data.ThumbnailImage);
+
+                    $('#txtTagM').val(data.Tags);
+                    $('#txtMetakeywordM').val(data.SeoKeywords);
+                    $('#txtMetaDescriptionM').val(data.SeoDescription);
+                    $('#txtSeoPageTitleM').val(data.SeoPageTitle);
+                    $('#txtSeoAliasM').val(data.SeoAlias);
+
+                    CKEDITOR.instances.txtContent.setData(data.Content);
+                    $('#ckStatusM').prop('checked', data.Status == 1);
+                    $('#ckHotM').prop('checked', data.HotFlag);
+                    $('#ckShowHomeM').prop('checked', data.HomeFlag);
+
+                    $('#modal-add-edit').modal('show');
+                    tedu.stopLoading();
+
+                },
+                error: function (status) {
+                    tedu.notify('Có lỗi xảy ra', 'error');
+                    tedu.stopLoading();
+                }
+            });
         });
+        $('body').on('click', '.btn-delete', function (e) {
+            e.preventDefault();
+            var that = $(this).data('id');
+            tedu.confirm('Are you sure to delete?', function () {
+                $.ajax({
+                    type: "POST",
+                    url: "/Admin/Product/Delete",
+                    data: { id: that },
+                    dataType: "json",
+                    beforeSend: function () {
+                        tedu.startLoading();
+                    },
+                    success: function (response) {
+                        tedu.notify('Delete successful', 'success');
+                        tedu.stopLoading();
+                        loadData();
+                    },
+                    error: function (status) {
+                        tedu.notify('Has an error in delete progress', 'error');
+                        tedu.stopLoading();
+                    }
+                });
+            });
+        });
+
+        $('#btnSave').on('click', function (e) {
+            if ($('#frmMaintainance').valid()) {
+                e.preventDefault();
+                var id = $('#hidIdM').val();
+                var name = $('#txtNameM').val();
+                var categoryId = $('#ddlCategoryIdM').combotree('getValue');
+
+                var description = $('#txtDescM').val();
+                var unit = $('#txtUnitM').val();
+
+                var price = $('#txtPriceM').val();
+                var originalPrice = $('#txtOriginalPriceM').val();
+                var promotionPrice = $('#txtPromotionPriceM').val();
+
+                //var image = $('#txtImageM').val();
+
+                var tags = $('#txtTagM').val();
+                var seoKeyword = $('#txtMetakeywordM').val();
+                var seoMetaDescription = $('#txtMetaDescriptionM').val();
+                var seoPageTitle = $('#txtSeoPageTitleM').val();
+                var seoAlias = $('#txtSeoAliasM').val();
+
+                var content = CKEDITOR.instances.txtContent.getData();
+                var status = $('#ckStatusM').prop('checked') == true ? 1 : 0;
+                var hot = $('#ckHotM').prop('checked');
+                var showHome = $('#ckShowHomeM').prop('checked');
+
+                $.ajax({
+                    type: "POST",
+                    url: "/Admin/Product/SaveEntity",
+                    data: {
+                        Id: id,
+                        Name: name,
+                        CategoryId: categoryId,
+                        Image: '',
+                        Price: price,
+                        OriginalPrice: originalPrice,
+                        PromotionPrice: promotionPrice,
+                        Description: description,
+                        Content: content,
+                        HomeFlag: showHome,
+                        HotFlag: hot,
+                        Tags: tags,
+                        Unit: unit,
+                        Status: status,
+                        SeoPageTitle: seoPageTitle,
+                        SeoAlias: seoAlias,
+                        SeoKeywords: seoKeyword,
+                        SeoDescription: seoMetaDescription
+                    },
+                    dataType: "json",
+                    beforeSend: function () {
+                        tedu.startLoading();
+                    },
+                    success: function (response) {
+                        tedu.notify('Update product successful', 'success');
+                        $('#modal-add-edit').modal('hide');
+                        resetFormMaintainance();
+
+                        tedu.stopLoading();
+                        loadData(true);
+                    },
+                    error: function () {
+                        tedu.notify('Has an error in save product progress', 'error');
+                        tedu.stopLoading();
+                    }
+                });
+                return false;
+            }
+
+        });
+
+    }
+
+    function registerControls() {
+        CKEDITOR.replace('txtContent', {});
+
+        //Fix: cannot click on element ck in modal
+        $.fn.modal.Constructor.prototype.enforceFocus = function () {
+            $(document)
+                .off('focusin.bs.modal') // guard against infinite focus loop
+                .on('focusin.bs.modal', $.proxy(function (e) {
+                    if (
+                        this.$element[0] !== e.target && !this.$element.has(e.target).length
+                        // CKEditor compatibility fix start.
+                        && !$(e.target).closest('.cke_dialog, .cke').length
+                        // CKEditor compatibility fix end.
+                    ) {
+                        this.$element.trigger('focus');
+                    }
+                }, this));
+        };
+
     }
     function initTreeDropDownCategory(selectedId) {
         $.ajax({
@@ -70,6 +232,7 @@
                 }
             }
         });
+    }
     function resetFormMaintainance() {
         $('#hidIdM').val(0);
         $('#txtNameM').val('');
@@ -94,6 +257,26 @@
         $('#ckStatusM').prop('checked', true);
         $('#ckHotM').prop('checked', false);
         $('#ckShowHomeM').prop('checked', false);
+
+    }
+    function loadCategories() {
+        $.ajax({
+            type: 'GET',
+            url: '/admin/product/GetAllCategories',
+            dataType: 'json',
+            success: function (response) {
+                var render = "<option value=''>--Select category--</option>";
+                $.each(response, function (i, item) {
+                    render += "<option value='" + item.Id + "'>" + item.Name + "</option>"
+                });
+                $('#ddlCategorySearch').html(render);
+            },
+            error: function (status) {
+                console.log(status);
+                tedu.notify('Cannot loading product category data', 'error');
+            }
+        });
+    }
     function loadData(isPageChanged) {
         var template = $('#table-template').html();
         var render = "";
