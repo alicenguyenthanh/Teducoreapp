7cd50b27939d6a89de1c384b19e7dedac111621d
Administrator <Administrator@WIN-A8E5KERI8HT>
2020-07-08 14:03:04 +0700

    Lesson47

--- TeduCoreApp.Application/Interfaces/IProductService.cs
+++ TeduCoreApp.Application/Interfaces/IProductService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TeduCoreApp.Application.ViewModels.Common;
 using TeduCoreApp.Application.ViewModels.Product;
 using TeduCoreApp.Utilities.Dtos;
 
@@ -38,5 +39,8 @@ namespace TeduCoreApp.Application.Interfaces
         List<ProductViewModel> GetLastest(int top);
 
         List<ProductViewModel> GetHotProduct(int top);
+        List<ProductViewModel> GetRelatedProducts(int id, int top);
+        List<ProductViewModel> GetUpsellProducts(int top);
+        List<TagViewModel> GetProductTags(int productId);
     }
 }
